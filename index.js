01f093c6a9d7f9f2c213b0b63d3ddf8842da37ba
kfconeone <kfconeone@gmail.com>
2021-06-10 16:17:58 +0800

    修正初始化問題

--- index.js
+++ index.js
@@ -1,62 +1,56 @@
+function getWidth() {
+    if (this._currentBrowserWidth == "none") {
+        let width = window.innerWidth > 0 ? window.innerWidth : screen.width;
+        if (width < 576) {
+            this._currentBrowserWidth = "xs";
+        } else if (width >= 576 && width < 768) {
+            this._currentBrowserWidth = "sm";
+        } else if (width >= 768 && width < 992) {
+            this._currentBrowserWidth = "md";
+        } else if (width >= 992 && width < 1200) {
+            this._currentBrowserWidth = "lg";
+        } else if (width >= 1200 && width < 1400) {
+            this._currentBrowserWidth = "xl";
+        } else if (width >= 1400 && width < 1920) {
+            this._currentBrowserWidth = "xxl";
+        } else if (width >= 1920) {
+            this._currentBrowserWidth = "xxxl";
+        }
+    } else return this._currentBrowserWidth;
+}
+
 export default {
     install: (app) => {
         let resizer = {
-            currentBrowserWidth: "none",
+            getWidth,
+            _currentBrowserWidth: "none",
             onBreak: {},
         };
         app.config.globalProperties.$resizer = resizer;
 
-        window.onload = () => {
-            let width = window.innerWidth > 0 ? window.innerWidth : screen.width;
-            if (width < 576) {
-                resizer.currentBrowserWidth = "xs";
-            } else if (width >= 576 && width < 768) {
-                resizer.currentBrowserWidth = "sm";
-            } else if (width >= 768 && width < 992) {
-                resizer.currentBrowserWidth = "md";
-            } else if (width >= 992 && width < 1200) {
-                resizer.currentBrowserWidth = "lg";
-            } else if (width >= 1200 && width < 1400) {
-                resizer.currentBrowserWidth = "xl";
-            } else if (width >= 1400 && width < 1920) {
-                resizer.currentBrowserWidth = "xxl";
-            } else if (width >= 1920) {
-                resizer.currentBrowserWidth = "xxxl";
-            }
-
-            /*
-            onbreak:{
-                section1: () => {},
-                section2: () => {}
-            }
-        
-        */
-        };
-
         let originResize = window.onresize;
-
         window.onresize = () => {
             if (originResize) originResize();
-            let originWidth = resizer.currentBrowserWidth;
+            let originWidth = resizer._currentBrowserWidth;
             let width = window.innerWidth > 0 ? window.innerWidth : screen.width;
-            if (width < 576 && resizer.currentBrowserWidth != "xs") {
-                resizer.currentBrowserWidth = "xs";
-            } else if (width >= 576 && width < 768 && resizer.currentBrowserWidth != "sm") {
-                resizer.currentBrowserWidth = "sm";
-            } else if (width >= 768 && width < 992 && resizer.currentBrowserWidth != "md") {
-                resizer.currentBrowserWidth = "md";
-            } else if (width >= 992 && width < 1200 && resizer.currentBrowserWidth != "lg") {
-                resizer.currentBrowserWidth = "lg";
-            } else if (width >= 1200 && width < 1400 && resizer.currentBrowserWidth != "xl") {
-                resizer.currentBrowserWidth = "xl";
-            } else if (width >= 1400 && width < 1920 && resizer.currentBrowserWidth != "xxl") {
-                resizer.currentBrowserWidth = "xxl";
-            } else if (width >= 1920 && resizer.currentBrowserWidth != "xxxl") {
-                resizer.currentBrowserWidth = "xxxl";
+            if (width < 576 && resizer._currentBrowserWidth != "xs") {
+                resizer._currentBrowserWidth = "xs";
+            } else if (width >= 576 && width < 768 && resizer._currentBrowserWidth != "sm") {
+                resizer._currentBrowserWidth = "sm";
+            } else if (width >= 768 && width < 992 && resizer._currentBrowserWidth != "md") {
+                resizer._currentBrowserWidth = "md";
+            } else if (width >= 992 && width < 1200 && resizer._currentBrowserWidth != "lg") {
+                resizer._currentBrowserWidth = "lg";
+            } else if (width >= 1200 && width < 1400 && resizer._currentBrowserWidth != "xl") {
+                resizer._currentBrowserWidth = "xl";
+            } else if (width >= 1400 && width < 1920 && resizer._currentBrowserWidth != "xxl") {
+                resizer._currentBrowserWidth = "xxl";
+            } else if (width >= 1920 && resizer._currentBrowserWidth != "xxxl") {
+                resizer._currentBrowserWidth = "xxxl";
             }
 
-            if (originWidth != resizer.currentBrowserWidth) {
-                Object.values(resizer.onBreak).forEach((f) => f(resizer.currentBrowserWidth));
+            if (originWidth != resizer._currentBrowserWidth) {
+                Object.values(resizer.onBreak).forEach((f) => f(resizer._currentBrowserWidth));
             }
         };
     },
